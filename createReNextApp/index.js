d2f41dc75083175b6cb98e815ebe04638473033b
GitAntinus <gantinus@gmail.com>
2018-01-07 00:30:32 +0800

    bug fixed

--- createReNextApp/index.js
+++ createReNextApp/index.js
@@ -1,7 +1,9 @@
-#!env node
 const fs = require('fs');
 const path = require('path');
 var program = require('commander');
+const execSync = require("child_process").execSync;
+const exec = require("child_process").exec;
+const npm = require("npm");
 
 program
   .version('0.0.1')
@@ -10,17 +12,24 @@ program
     fs.stat( projectName , ( err , result ) => {
       if( err ){
         fs.mkdirSync( path.resolve( './' , projectName ) );
-        copyDir( __dirname + '/resources' , path.resolve( './' , projectName ) );
+        copyDir( __dirname + '/resources' , path.resolve( './' , projectName )  , "" );
+        process.chdir( projectName );
+        exec( "npm install renext" , (error, stdout, stderr) => {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+        })
       }
       else {
         console.log( projectName + " already exist");
-        console.log( projectName + " is a file");
       }
     })
   })
   .parse( process.argv );
 
-function copyDir( src , dest ){
+function copyDir( src , dest , path ){
   const files = fs.readdirSync( src );
   for( let i = 0 ; i < files.length ; i++ ){
     if( fs.statSync( src + '/' + files[i] ).isDirectory() ){
@@ -28,9 +37,8 @@ function copyDir( src , dest ){
       copyDir( src + '/' + files[i] , dest + '/' + files[i] );
     }
     else {
-      fs.copyFile( src + '/' + files[i] , dest + '/' + files[i] , () =>{
-        console.log( 'creating ' + src + '/' + files[i] );
-      });
+      fs.copyFileSync( src + '/' + files[i] , dest + '/' + files[i] );
+      console.log( "created " + files[i] );
     }
   }
 }
