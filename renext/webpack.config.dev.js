9f6da2e5cc038b4d149ab9bcdbb60a4b6362aa68
GitAntinus <gantinus@gmail.com>
2018-01-26 22:52:30 +0800

    bug fixed

--- renext/webpack.config.dev.js
+++ renext/webpack.config.dev.js
@@ -49,7 +49,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /node_modules\/renext-?.*\/.*\.(react|js)$/,
+        test: /renext.*\.(react|js)$/,
         use: {
           loader: "babel-loader"
         }
