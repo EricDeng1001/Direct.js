89ef6f6bb61627ac84565f3816101eeba3ba6240
GitAntinus <gantinus@gmail.com>
2018-04-04 10:45:39 +0800

    this is a quick commit

--- direct-start/Algorithm/mergeDir.js
+++ direct-start/Algorithm/mergeDir.js
@@ -3,6 +3,11 @@ module.exports = function mergeDir( src , dest , path ){
   const files = fs.readdirSync( src );
   for( let i = 0 ; i < files.length ; i++ ){
     if( fs.statSync( src + "/" + files[i] ).isDirectory() ){
+      try {
+        fs.statSync( src + "/" + files[i] )
+      } catch( e ){
+        fs.mkdirSync( dest + "/" + files[i] );
+      }
       mergeDir( src + "/" + files[i] , dest + "/" + files[i] , path + "/" + files[i] );
       console.log( "merged " + path + "/" + files[i] );
     }
