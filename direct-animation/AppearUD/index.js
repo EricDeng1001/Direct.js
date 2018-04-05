1e937bd9775457d2bee756399c9cdc00f542e746
GitAntinus <gantinus@gmail.com>
2018-04-05 21:56:58 +0800

    the animation lib for direct

---
+++ direct-animation/AppearUD/index.js
@@ -0,0 +1,9 @@
+import makeAnimation from 'direct-core/makeAnimation';
+import appearUD from 'style';
+
+const timeout = {
+  enter: 1000,
+  exit: 500
+};
+
+export default makeAnimation( appearUD , timeout );
---
+++ direct-animation/SlideDU/index.js
@@ -0,0 +1,9 @@
+import makeAnimation from 'direct-core/makeAnimation';
+import slideDU from 'style';
+
+const timeout = {
+  enter: 1000,
+  exit: 1000
+};
+
+export default makeAnimation( slideDU , timeout );
