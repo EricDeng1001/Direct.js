ed6adaab87f1d1def4f9676ff94c21cecd8fbf3e
GitAntinus <gantinus@gmail.com>
2018-04-05 21:54:52 +0800

    rewrite merge, now merge( a , b ) will keep a\'s prop\'s value, instead of replace it with b's

--- direct-core/Animation/SlideUD/index.js
+++
@@ -1,9 +0,0 @@
-import makeAnimation from '../../makeAnimation';
-import slideUD from 'style';
-
-const timeout = {
-  enter: 1000,
-  exit: 1000
-};
-
-export default makeAnimation( slideUD , timeout );
