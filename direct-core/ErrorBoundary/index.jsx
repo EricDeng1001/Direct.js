47350855c49dff34b9d9a9de8c48159d3d21474c
GitAntinus <gantinus@gmail.com>
2018-04-09 00:01:01 +0800

    updated for 1.5.0

---
+++ direct-core/ErrorBoundary/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  static defaultProps = {
+    handler: () => null
+  }
+  state = { hasError: false }
+
+  componentDidCatch( error, info ){
+    const { handler , showErrorMesaage } = this.props;
+    showErrorMesaage ?
+    this.setState({ hasError: true , error , info })
+    :this.setState({ hasError: true });
+    handler( error, info );
+  }
+
+  render() {
+    const { children , showErrorMesaage } = this.props;
+    const { error , info , hasError } = this.state;
+    if ( hasError ){
+      return (
+        <React.Fragment>
+          <h1>Something went wrong.</h1>
+          {
+            showErrorMesaage ? (
+              <div>
+                <h2>error:</h2>
+                <p>{error}</p>
+                <h2>info:</h2>
+                <p>{info}</p>
+              </div>
+            )
+            : null
+          }
+        </React.Fragment>
+      );
+    }
+    return children;
+  }
+};
+
+export default ErrorBoundary;
