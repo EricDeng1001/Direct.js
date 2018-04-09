1298dec2c844ee448a27ede328b8a88eba0ebc5b
Eric Deng <gantinus@gmail.com>
2018-04-09 13:57:00 +0800

    Update routes.js
    async load config

--- direct/App/routes.js
+++ direct/App/routes.js
@@ -2,50 +2,55 @@ import React from "react";
 
 import { Route , Switch , Redirect } from "react-router-dom";
 
-import routesConfig from "Config/routes";
-
-import AppConfig from "Config/App";
-
 import ErrorBoundary from "direct-core/ErrorBoundary";
 
-const paths = Object.keys( routesConfig );
+var Routes;
 
-const theRoutes = paths.map( path => {
-  let tmp = routesConfig[path];
-  if( tmp.redirect ){
-    return (
-      <Redirect
-        key={tmp.from + tmp.to}
-        from={path}
-        to={tmp.redirect}
-        exact={tmp.exact}
-      />
-    );
-  } else {
-    return (
-      <Route
-        key={path}
-        path={path}
-        exact={!tmp.nested}
-        component={tmp.page}
-      />
-    );
-  }
-});
+async function loadConfig(){
+  const routesConfigPromise = import("Config/routes");
+  const AppConfigPromise = import("Config/App");
+  const routesConfig = (await routesConfigPromise).default;
+  const AppConfig = (await AppConfigPromise).default;
+  const paths = Object.keys( routesConfig );
 
-class Routes extends React.PureComponent {
-  render() {
-    return (
-      <ErrorBoundary
-        showErrorMessage={AppConfig.onUIErrorShowErrorMessage}
-        errorHandler={AppConfig.UIErrorHandler}
-      >
-        <Switch location={this.props.location}>
-          {theRoutes}
-        </Switch>
-      </ErrorBoundary>
-    );
+  const theRoutes = paths.map( path => {
+    let tmp = routesConfig[path];
+    if( tmp.redirect ){
+      return (
+        <Redirect
+          key={tmp.from + tmp.to}
+          from={path}
+          to={tmp.redirect}
+          exact={tmp.exact}
+        />
+      );
+    } else {
+      return (
+        <Route
+          key={path}
+          path={path}
+          exact={!tmp.nested}
+          component={tmp.page}
+        />
+      );
+    }
+  });
+
+ Routes = class extends React.PureComponent {
+    render() {
+      return (
+        <ErrorBoundary
+          showErrorMessage={AppConfig.onUIErrorShowErrorMessage}
+          errorHandler={AppConfig.UIErrorHandler}
+        >
+          <Switch location={this.props.location}>
+            {theRoutes}
+          </Switch>
+        </ErrorBoundary>
+      );
+    }
   }
 }
 
+loadConfig();
 export default Routes;
