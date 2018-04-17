4b7fe5c30be189be4ed085cde040f7688d92a553
GitAntinus <gantinus@gmail.com>
2018-04-18 00:00:35 +0800

    this is a quick commit

--- simple-direct/App/socket.js
+++ simple-direct/App/socket.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client';
 
-import { socket as socketConfig } from "Config/App";
+import { socket as socketConfig } from "Core/App";
+
 var socket = io( socketConfig );
 
 socket.io.opts.transports = ['polling', 'websocket'];
